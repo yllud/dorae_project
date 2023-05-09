501ab9bb8c5766ddbddee6c7f388237cf300be4e
ChoiHyunSuk93 <ChoiHyunSuk93@github.com>
2023-05-09 14:25:05 +0900

    미니프로젝트파일

--- dorae/src/main/java/com/multi/dorae/sns/BlogVO.java
+++ dorae/src/main/java/com/multi/dorae/sns/BlogVO.java
@@ -1,5 +1,47 @@
 package com.multi.dorae.sns;
 
 public class BlogVO {
-
+    private String title;
+    private String link;
+    private String description;
+    private String bloggername;
+    private String bloggerlink;
+    private String postdate;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getBloggername() {
+        return bloggername;
+    }
+    public void setBloggername(String bloggername) {
+        this.bloggername = bloggername;
+    }
+    public String getBloggerlink() {
+        return bloggerlink;
+    }
+    public void setBloggerlink(String bloggerlink) {
+        this.bloggerlink = bloggerlink;
+    }
+    public String getPostdate() {
+        return postdate;
+    }
+    public void setPostdate(String postdate) {
+        this.postdate = postdate;
+    }
 }
