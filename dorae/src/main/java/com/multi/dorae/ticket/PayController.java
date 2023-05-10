c02ab74d215365f28a9a7d2b5fe8a9f6a8d07591
ARAM <xiachestnut@naver.com>
2023-05-10 16:14:31 +0900

    예매페이지 관련 DAO, Controller 추가 one.jsp >> 예매상세페이지 (아직 미완성)

--- dorae/src/main/java/com/multi/dorae/ticket/PayController.java
+++ dorae/src/main/java/com/multi/dorae/ticket/PayController.java
@@ -1,5 +1,13 @@
 package com.multi.dorae.ticket;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PayController {
 
+	@Autowired
+	PayDAO dao;
+	
+	
 }
--- dorae/src/main/java/com/multi/dorae/ticket/SeatDAO.java
+++ dorae/src/main/java/com/multi/dorae/ticket/SeatDAO.java
@@ -1,5 +1,23 @@
 package com.multi.dorae.ticket;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.multi.dorae.search.PlayVO;
+
+@Component
 public class SeatDAO {
 
+	@Autowired
+	SqlSessionTemplate my;
+	
+	// 공연 id로 공연 한개 검색(예매페이지)
+		public PlayVO one(String play_id) {
+			System.out.println(play_id);
+			PlayVO vo = my.selectOne("play.one", play_id);
+
+			return vo;
+
+		}
 }
