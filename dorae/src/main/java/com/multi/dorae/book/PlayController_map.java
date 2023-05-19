f0c1494364deed5e1c014ff0df7f5099e7486a4b
JangHJ <yg9316@swu.ac.kr>
2023-05-19 17:16:00 +0900

    Controller, mapper 변동사항없는걸로 다시 커밋

---
+++ dorae/src/main/java/com/multi/dorae/book/PlayController_map.java
@@ -0,0 +1,61 @@
+package com.multi.dorae.book;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.multi.dorae.search.PlayDAO;
+import com.multi.dorae.search.PlayVO;
+import com.multi.dorae.search.RankDAO;
+import com.multi.dorae.search.StageDAO;
+import com.multi.dorae.search.StageVO;
+
+@Controller
+public class PlayController_map {
+
+	@Autowired
+	PlayDAO dao;
+
+	@Autowired
+	StageDAO dao2;
+	
+	@Autowired
+	RankDAO dao3;
+
+	@RequestMapping(value = "map/infoList", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public String infoList() throws Exception {
+	    System.out.println("infoList");
+	    List<String> idList = dao.listPlayId();
+	    List<String> stList = new ArrayList<>();
+	    List<PlayVO> delist1 = new ArrayList<>(); // play detail
+	    List<StageVO> delist2 = new ArrayList<>(); // stage detail
+
+	    for(int i=0; i<idList.size(); i++) {
+	        stList.add(dao.stageId(idList.get(i)));
+	        delist1.add(dao.playDetail(idList.get(i)));
+	    }
+	    for(int i=0; i<stList.size(); i++) {
+	        delist2.add(dao2.stageDetail(stList.get(i)));
+	    }
+	    // JSON 변환을 위한 ObjectMapper 생성
+	    ObjectMapper objectMapper = new ObjectMapper();
+	    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+	    // delist1과 delist2를 Map으로 묶어서 JSON으로 변환
+	    Map<String, Object> data = new HashMap<>();
+	    data.put("delist1", delist1);
+	    data.put("delist2", delist2);
+	    String json = objectMapper.writeValueAsString(data);
+	  
+	    //System.out.println(json);
+	    return json;
+	}
+}
