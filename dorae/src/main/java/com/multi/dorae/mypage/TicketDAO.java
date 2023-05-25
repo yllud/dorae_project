bfefc4bf9732b275efae7bd3a8a73db98c0c21f5
DESKTOP-TFP5N8I\67 <yunjin9672@gamil.com>
2023-05-25 16:42:43 +0900

    naverMapper 관리자추가

--- dorae/src/main/java/com/multi/dorae/mypage/TicketDAO.java
+++ dorae/src/main/java/com/multi/dorae/mypage/TicketDAO.java
@@ -16,14 +16,14 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//테이블 하나당 DAO하나! ==> CUD를 완성!! 
 @Component
 public class TicketDAO {
 	
 	@Autowired
 	SqlSessionTemplate my;
 
-
+	// seat, pay, play 테이블을 join한 것 전부 불러오는 list
+	// 해당하는 email을 전부 불러옴
 	 public List<TicketVO> listByEmail(String email) {
 		 List<TicketVO> list = my.selectList("mypageList.ticketJoin", email);
 		    return list;
