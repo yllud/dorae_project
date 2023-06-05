d1cfca92ad4b4f2f37eadc7ae6b0e7cd547d4121
JangHJ <yg9316@swu.ac.kr>
2023-06-06 02:24:04 +0900

    추천 sql문 + service 필터링 수정중 1

--- dorae/src/main/java/com/multi/dorae/map/PreferenceService.java
+++ dorae/src/main/java/com/multi/dorae/map/PreferenceService.java
@@ -36,85 +36,125 @@ public class PreferenceService implements PreferenceServiceInterface{
 	public List<PlayVO> getRecommendedPlays(PreferenceVO bag) {
         List<PlayVO> recommendedPlays = dao.getRecommendedPlays(bag);
         List<StageVO> recommendedStages = new ArrayList<>();
+        int count = 0;
         
         for(PlayVO vo : recommendedPlays) {
         	recommendedStages.add(dao2.stageDetail(vo.getStage_id()));
         }
-        // 필요한 추가 로직을 수행하거나 필터링을 적용할 수 있습니다.
+        // 필요한 추가 로직을 수행하거나 필터링을 적용
         List<PlayVO> filteredPlays = new ArrayList<>();
         
         // Filter by first priority (genre1 and area1 matching)
         for (int i = 0; i < recommendedPlays.size(); i++) {
             PlayVO play = recommendedPlays.get(i);
             StageVO stage = recommendedStages.get(i);
-
-            if (play.getGenre_name().equals(bag.getGenre1()) && stage.getAddress().startsWith(bag.getArea1())) {
+            if (bag.getGenre1() != null && play.getGenre_name().equals(bag.getGenre1()) && bag.getArea1() != null && stage.getAddress().startsWith(bag.getArea1())) {
                 filteredPlays.add(play);
-                if (filteredPlays.size() >= 15) {
+                count++;
+                if (count >= 10) {
                     break;
                 }
             }
         }
-
-        // Filter by second priority (genre1 and area2 matching)
+        count = 0;
+        // Filter by second priority (genre2 and area1 matching)
         for (int i = 0; i < recommendedPlays.size(); i++) {
             PlayVO play = recommendedPlays.get(i);
             StageVO stage = recommendedStages.get(i);
-
-            if (play.getGenre_name().equals(bag.getGenre1()) && stage.getAddress().startsWith(bag.getArea2())) {
+            if (bag.getGenre2() != null && play.getGenre_name().equals(bag.getGenre2()) && bag.getArea1() != null && stage.getAddress().startsWith(bag.getArea1())) {
                 filteredPlays.add(play);
-                if (filteredPlays.size() >= 20) {
+                count++;
+                if (count >= 8) {
                     break;
                 }
             }
         }
-
-        // Filter by third priority (genre2 and area1 matching)
+        count = 0;
+        // Filter by third priority (genre1 and area2 matching)
         for (int i = 0; i < recommendedPlays.size(); i++) {
             PlayVO play = recommendedPlays.get(i);
             StageVO stage = recommendedStages.get(i);
-
-            if (play.getGenre_name().equals(bag.getGenre2()) && stage.getAddress().startsWith(bag.getArea1())) {
+            if (bag.getGenre1() != null && play.getGenre_name().equals(bag.getGenre1()) && bag.getArea2() != null && stage.getAddress().startsWith(bag.getArea2())) {
                 filteredPlays.add(play);
-                if (filteredPlays.size() >= 20) {
+                count++;
+                if (count >= 6) {
                     break;
                 }
             }
         }
-
+        count = 0;
+        // Filter by third priority (genre3 and area1 matching)
+        for (int i = 0; i < recommendedPlays.size(); i++) {
+        	PlayVO play = recommendedPlays.get(i);
+        	StageVO stage = recommendedStages.get(i);
+        	 if (bag.getGenre3() != null && play.getGenre_name().equals(bag.getGenre3()) && bag.getArea1() != null && stage.getAddress().startsWith(bag.getArea1())) {
+        		filteredPlays.add(play);
+        		count++;
+        		if (count >= 5) {
+        			break;
+        		}
+        	}
+        }
+        count = 0;
         // Filter by fourth priority (genre2 and area2 matching)
         for (int i = 0; i < recommendedPlays.size(); i++) {
             PlayVO play = recommendedPlays.get(i);
             StageVO stage = recommendedStages.get(i);
-
-            if (play.getGenre_name().equals(bag.getGenre2()) && stage.getAddress().startsWith(bag.getArea2())) {
+            if (bag.getGenre2() != null && play.getGenre_name().equals(bag.getGenre2()) && bag.getArea2() != null && stage.getAddress().startsWith(bag.getArea2())) {
                 filteredPlays.add(play);
-                if (filteredPlays.size() >= 25) {
+                count++;
+                if (count >= 4) {
                     break;
                 }
             }
         }
-
-        // Filter by remaining plays (genre3 and area1 matching)
+        count = 0;
+        // Filter by remaining plays (genre3 and area2 matching)
         for (int i = 0; i < recommendedPlays.size(); i++) {
             PlayVO play = recommendedPlays.get(i);
             StageVO stage = recommendedStages.get(i);
-
-            if (play.getGenre_name().equals(bag.getGenre3()) && stage.getAddress().startsWith(bag.getArea1())) {
+            if (bag.getGenre3() != null && play.getGenre_name().equals(bag.getGenre3()) && bag.getArea2() != null && stage.getAddress().startsWith(bag.getArea2())) {
                 filteredPlays.add(play);
-                if (filteredPlays.size() >= 30) {
+                count++;
+                if (count >= 3) {
                     break;
                 }
             }
+        }
+        count = 0;
+        // Filter by fourth priority (genre1 and area3 matching)
+        for (int i = 0; i < recommendedPlays.size(); i++) {
+        	PlayVO play = recommendedPlays.get(i);
+        	StageVO stage = recommendedStages.get(i);
+        	 if (bag.getGenre1() != null && play.getGenre_name().equals(bag.getGenre1()) && bag.getArea3() != null && stage.getAddress().startsWith(bag.getArea3())) {
+        		filteredPlays.add(play);
+        		count++;
+        		if (count >=2) {
+        			break;
+        		}
+        	}
+        }
+        count = 0;
+        // Filter by remaining plays (genre2 and area3 matching)
+        for (int i = 0; i < recommendedPlays.size(); i++) {
+        	PlayVO play = recommendedPlays.get(i);
+        	StageVO stage = recommendedStages.get(i);
+        	 if (bag.getGenre2() != null && play.getGenre_name().equals(bag.getGenre2()) && bag.getArea3() != null && stage.getAddress().startsWith(bag.getArea3())) {
+        		filteredPlays.add(play);
+        		count++;
+        		if (count >= 2) {
+        			break;
+        		}
+        	}
         }
 
         // Add remaining plays without specific filters
         for (int i = 0; i < recommendedPlays.size(); i++) {
             PlayVO play = recommendedPlays.get(i);
-
             if (!filteredPlays.contains(play)) {
                 filteredPlays.add(play);
-                if (filteredPlays.size() >= 35) {
+                count++;
+                if (count >= 2) {
                     break;
                 }
             }
